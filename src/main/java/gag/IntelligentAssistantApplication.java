fba5bcc2b2fa588f49f0bce056baca1883cf0fb6
gag <908239167@qq.com>
2026-05-15 00:09:17 +0800

    新建项目

---
+++ src/main/java/gag/IntelligentAssistantApplication.java
@@ -0,0 +1,13 @@
+package gag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntelligentAssistantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntelligentAssistantApplication.class, args);
+    }
+
+}
