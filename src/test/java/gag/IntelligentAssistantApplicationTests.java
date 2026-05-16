e04de5fb1815805d0307e3f824d95d86190ac6a0
gag <908239167@qq.com>
2026-05-16 15:24:41 +0800

    提交文件

---
+++ src/test/java/gag/IntelligentAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package gag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntelligentAssistantApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
